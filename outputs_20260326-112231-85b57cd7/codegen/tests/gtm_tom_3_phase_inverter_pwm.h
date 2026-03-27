6ef4da0ee18d0f0072b612892cf58c54ac7b8ffe
Don Joacquin Perez <kinaiprojects@gmail.com>
2026-03-27 10:54:27 +0800

    trace: tdd phase=test phase_attempt=4 attempt=4 status=failure

--- outputs_20260326-112231-85b57cd7/codegen/tests/gtm_tom_3_phase_inverter_pwm.h
+++ outputs_20260326-112231-85b57cd7/codegen/tests/gtm_tom_3_phase_inverter_pwm.h
@@ -1,11 +1,11 @@
 /*
  * gtm_tom_3_phase_inverter_pwm.h
- * Public API for GTM TOM 3-Phase Inverter PWM driver (TC3xx)
+ * Public API for 3-phase inverter PWM using IfxGtm_Pwm (TOM1, center-aligned)
  */
 #ifndef GTM_TOM_3_PHASE_INVERTER_PWM_H
 #define GTM_TOM_3_PHASE_INVERTER_PWM_H
 
-/* Public APIs */
+/* Public API */
 void initGtmTom3phInv(void);
 void updateGtmTom3phInvDuty(void);
 
