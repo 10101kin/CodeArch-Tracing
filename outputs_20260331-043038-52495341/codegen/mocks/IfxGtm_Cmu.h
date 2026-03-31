5fcb0a3f4af15c0ae3aba10df260ce8b766c7d51
Don Joacquin Perez <kinaiprojects@gmail.com>
2026-03-31 12:46:01 +0800

    trace: tdd phase=mock_generation phase_attempt=1 attempt=1 status=success

---
+++ outputs_20260331-043038-52495341/codegen/mocks/IfxGtm_Cmu.h
@@ -0,0 +1,58 @@
+#ifndef IFXGTM_CMU_H
+#define IFXGTM_CMU_H
+
+#include "mock_gtm_tom_3_phase_inverter_pwm.h"
+
+/* CMU clock enums */
+typedef enum
+{
+    IfxGtm_Cmu_Clk_0 = 0,
+    IfxGtm_Cmu_Clk_1,
+    IfxGtm_Cmu_Clk_2,
+    IfxGtm_Cmu_Clk_3,
+    IfxGtm_Cmu_Clk_4,
+    IfxGtm_Cmu_Clk_5,
+    IfxGtm_Cmu_Clk_6,
+    IfxGtm_Cmu_Clk_7
+} IfxGtm_Cmu_Clk;
+
+typedef enum
+{
+    IfxGtm_Cmu_Eclk_0 = 0,
+    IfxGtm_Cmu_Eclk_1,
+    IfxGtm_Cmu_Eclk_2
+} IfxGtm_Cmu_Eclk;
+
+typedef enum
+{
+    IfxGtm_Cmu_Fxclk_0 = 0,
+    IfxGtm_Cmu_Fxclk_1,
+    IfxGtm_Cmu_Fxclk_2,
+    IfxGtm_Cmu_Fxclk_3,
+    IfxGtm_Cmu_Fxclk_4
+} IfxGtm_Cmu_Fxclk;
+
+typedef enum
+{
+    IfxGtm_Cmu_Tim_Filter_Clk_0,
+    IfxGtm_Cmu_Tim_Filter_Clk_1,
+    IfxGtm_Cmu_Tim_Filter_Clk_6,
+    IfxGtm_Cmu_Tim_Filter_Clk_7
+} IfxGtm_Cmu_Tim_Filter_Clk;
+
+#ifndef IFXGTM_CMU_CLKEN_FXCLK
+#define IFXGTM_CMU_CLKEN_FXCLK (0x1U)
+#endif
+#ifndef IFXGTM_CMU_CLKEN_CLK0
+#define IFXGTM_CMU_CLKEN_CLK0  (0x2U)
+#endif
+
+/* Mandatory CMU functions */
+void    IfxGtm_Cmu_enable(Ifx_GTM *module);
+boolean IfxGtm_Cmu_isEnabled(Ifx_GTM *module);
+float32 IfxGtm_Cmu_getModuleFrequency(Ifx_GTM *gtm);
+void    IfxGtm_Cmu_setGclkFrequency(Ifx_GTM *gtm, float32 frequency);
+void    IfxGtm_Cmu_enableClocks(Ifx_GTM *gtm, uint32 clkMask);
+void    IfxGtm_Cmu_setClkFrequency(Ifx_GTM *gtm, IfxGtm_Cmu_Clk clkIndex, float32 frequency);
+
+#endif /* IFXGTM_CMU_H */
