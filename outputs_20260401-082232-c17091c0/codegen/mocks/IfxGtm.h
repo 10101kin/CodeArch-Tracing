48a0d1e81212d9cf2a01d86a37955b37ff1a64c3
Don Joacquin Perez <kinaiprojects@gmail.com>
2026-04-01 16:40:27 +0800

    trace: tdd phase=mock_generation phase_attempt=1 attempt=1 status=success

---
+++ outputs_20260401-082232-c17091c0/codegen/mocks/IfxGtm.h
@@ -0,0 +1,26 @@
+#ifndef IFXGTM_H
+#define IFXGTM_H
+
+#include "mock_gtm_tom_3_phase_inverter_pwm_2.h"
+
+/* Enums */
+typedef enum
+{
+    IfxGtm_IrqMode_level       = 0,
+    IfxGtm_IrqMode_pulse       = 1,
+    IfxGtm_IrqMode_pulseNotify = 2,
+    IfxGtm_IrqMode_singlePulse = 3
+} IfxGtm_IrqMode;
+
+typedef enum
+{
+    IfxGtm_SuspendMode_none = 0,
+    IfxGtm_SuspendMode_hard = 1,
+    IfxGtm_SuspendMode_soft = 2
+} IfxGtm_SuspendMode;
+
+/* Functions (subset needed by module) */
+boolean IfxGtm_isEnabled(Ifx_GTM *gtm);
+void    IfxGtm_enable(Ifx_GTM *gtm);
+
+#endif /* IFXGTM_H */
