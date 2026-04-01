9edb2a9aa30706e4bfa84bb7a7b4c31fd13fb2cc
Don Joacquin Perez <kinaiprojects@gmail.com>
2026-04-01 17:28:03 +0800

    trace: tdd phase=production phase_attempt=5 attempt=11 status=failure

--- outputs_20260401-083520-84c756d6/codegen/production/egtm_atom_3_phase_inverter_pwm.h
+++ outputs_20260401-083520-84c756d6/codegen/production/egtm_atom_3_phase_inverter_pwm.h
@@ -1,12 +1,20 @@
 /*
  * egtm_atom_3_phase_inverter_pwm.h
- * Public API for EGTM ATOM 3-Phase Inverter PWM driver
+ *
+ * Public API for EGTM ATOM 3-Phase Inverter PWM driver (TC4xx).
  */
 #ifndef EGTM_ATOM_3_PHASE_INVERTER_PWM_H
 #define EGTM_ATOM_3_PHASE_INVERTER_PWM_H
 
-/* Public API */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void initEgtmAtom3phInv(void);
 void updateEgtmAtom3phInvDuty(float32 *requestDuty);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* EGTM_ATOM_3_PHASE_INVERTER_PWM_H */
