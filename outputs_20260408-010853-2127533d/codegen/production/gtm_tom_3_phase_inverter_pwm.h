8b75f497ad021429b410c84ee4903f45ae0c9964
Don Joacquin Perez <kinaiprojects@gmail.com>
2026-04-08 09:59:10 +0800

    trace: tdd phase=production phase_attempt=3 attempt=5 status=success

--- outputs_20260408-010853-2127533d/codegen/production/gtm_tom_3_phase_inverter_pwm.h
+++ outputs_20260408-010853-2127533d/codegen/production/gtm_tom_3_phase_inverter_pwm.h
@@ -1,22 +1,14 @@
 /*
  * gtm_tom_3_phase_inverter_pwm.h
- * Public API for GTM TOM 3-Phase Inverter PWM using unified IfxGtm_Pwm driver
+ * Public API for GTM TOM 3-Phase Inverter PWM (IfxGtm_Pwm)
  */
 #ifndef GTM_TOM_3_PHASE_INVERTER_PWM_H
 #define GTM_TOM_3_PHASE_INVERTER_PWM_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/* Public API */
+/* Public API prototypes */
 void initGtmTom3phInv(void);
 void updateGtmTom3phInvDuty(void);
 void interruptGtmAtom(void);
 void IfxGtm_periodEventFunction(void *data);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* GTM_TOM_3_PHASE_INVERTER_PWM_H */
